5e2dd5c73396ca29dbeb5c9ccad2e56a4117c4eb
Jonathan BG <bombolti_02@hotmail.com>
2019-10-31 17:48:48 +0000

    changes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,22 +2,22 @@ import mongoose from 'mongoose';
 
 export default class MongoDB{
 
-   private uri = ('mongodb+srv://amdin:wass123456%40@cluster0-xvo62.mongodb.net/ionic-api?retryWrites=true&w=majority'||process.env.DATABASE_URI); 
+   private uri = process.env.DATABASE_URI;
    constructor(){}
-   
+
    async startDB(){
     try {
         await mongoose.connect(this.uri, {
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true
-        }); 
-        console.log('Database is connected')   
+        });
+        console.log('Database is connected')
     } catch (error) {
         console.error(error);
     }
-    
+
    }
-   
+
 
 }
